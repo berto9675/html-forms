26473f9835f9120269d27e4688c198e37056b936
berto9675 <albrtvd@.com>
2025-01-13 18:23:57 +0100

    Update Se quitan lo comentarios

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const form = document.getElementById("surveyForm");
 form.addEventListener('submit', function(event){
     event.preventDefault(); 
 
-
     const formData = new FormData(form);
 
- 
     const name = formData.get('name');
     const email = formData.get('email');
     const age = formData.get('age');
@@ -15,12 +13,10 @@ form.addEventListener('submit', function(event){
     const recommend = formData.get('recommend');
     const comment = form.querySelector('#comment').value; 
 
-
     const languages = [];
     document.querySelectorAll("input[name='languages']:checked").forEach(function(checkbox){
         languages.push(checkbox.value);
     });
-
 
     console.log("Nombre", name);
     console.log("Email", email);
