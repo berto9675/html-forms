c97467d435238aa517ab0a1cebb03fe0e6c5beeb
berto9675 <albrtvd@.com>
2025-01-13 18:20:46 +0100

    Upadte Se quitan comentarios

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-//Seleccionamos el formulario por su ID
+
 const form = document.getElementById("surveyForm");
 
-//Agregrar evento 'submit' al formulario
 form.addEventListener('submit', function(event){
     event.preventDefault(); 
 
-    //Obtenemos los datos del formulario
+
     const formData = new FormData(form);
 
-    //Extraemos los valores 
+ 
     const name = formData.get('name');
     const email = formData.get('email');
     const age = formData.get('age');
@@ -16,13 +15,13 @@ form.addEventListener('submit', function(event){
     const recommend = formData.get('recommend');
     const comment = form.querySelector('#comment').value; 
 
-    //Recogemos los valores de los checkboxes seleccionados
+
     const languages = [];
     document.querySelectorAll("input[name='languages']:checked").forEach(function(checkbox){
         languages.push(checkbox.value);
     });
 
-    //Mostramos los datos en la consola
+
     console.log("Nombre", name);
     console.log("Email", email);
     console.log("Edad", age);
